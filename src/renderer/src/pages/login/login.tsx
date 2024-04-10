e815433f82a192ad26c4383c830429cf93c7db5b
xulibang <xulibangyaojingjing@gmail.com>
2024-04-10 14:22:40 +0800

    feat: add logo

--- src/renderer/src/pages/login/login.tsx
+++ src/renderer/src/pages/login/login.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@renderer/base.css'
 
+function Login(): JSX.Element {
+  return <div>login</div>
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>login</div>
+    <Login />
   </React.StrictMode>
 )
