c75e691bfcaa1571b9977147b27351b6244c78b6
xulibang <xulibangyaojingjing@gmail.com>
2024-04-10 19:27:24 +0800

    feat: update ui

--- src/renderer/src/pages/login/login.tsx
+++ src/renderer/src/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Button } from '@mui/material'
+import { Button, FormControl, TextField, Grid } from '@mui/material'
+import logo from '@renderer/assets/icon.png'
 import '@renderer/base.css'
 
 function Login(): JSX.Element {
@@ -17,9 +18,22 @@ function Login(): JSX.Element {
   }
 
   return (
-    <div>
-      <Button onClick={handleLogin}>登录</Button>
-    </div>
+    <FormControl>
+      <Grid paddingTop={4} container spacing={2} textAlign="center" alignItems="center">
+        <Grid item xs={12}>
+          <img width={80} src={logo}></img>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField size="small" id="name" label="name" variant="outlined" />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField size="small" id="password" label="password" variant="outlined" />
+        </Grid>
+        <Grid item xs={12}>
+          <Button onClick={handleLogin}>登录</Button>
+        </Grid>
+      </Grid>
+    </FormControl>
   )
 }
 
