e000661994c3b693c4c007fb77058d9121057437
xulibang <xulibangyaojingjing@gmail.com>
2024-04-12 17:59:51 +0800

    feat: upload FIle

--- src/renderer/src/pages/main/pages/home/index.tsx
+++ src/renderer/src/pages/main/pages/home/index.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react'
-import { InboxOutlined } from '@ant-design/icons'
+import { useEffect, useState } from 'react'
 import type { UploadProps } from 'antd'
 import { message, Upload } from 'antd'
 import { useGlobalStore } from '@renderer/store/global'
@@ -9,20 +8,25 @@ import styles from './index.module.less'
 
 export default function Main(): JSX.Element {
   const setUserInfo = useGlobalStore((state) => state.setUserInfo)
+  const [imageSrc, setImageSrc] = useState<string>()
 
   const { ipcRenderer } = window.electron
 
   const props: UploadProps = {
     name: 'file',
     multiple: true,
-    // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+    showUploadList: false,
     onChange(info) {
       const { status } = info.file
       if (status !== 'uploading') {
         console.log(info.file, info.fileList)
       }
       if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`)
+        const reader = new FileReader()
+        reader.readAsDataURL(info.file.originFileObj as Blob)
+        reader.onload = (): void => {
+          setImageSrc(reader.result as string)
+        }
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`)
       }
@@ -36,20 +40,17 @@ export default function Main(): JSX.Element {
       setUserInfo(data)
     }
   }
+
+  const imgStyle: React.CSSProperties = {
+    width: '50%'
+  }
   useEffect(() => {
     ipcRenderer.on('main', handleMessageFromMain)
   }, [])
   return (
     <div className={styles.home}>
       <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibited from uploading company data or
-          other banned files.
-        </p>
+        <img style={imgStyle} src={imageSrc} />
       </Dragger>
     </div>
   )
